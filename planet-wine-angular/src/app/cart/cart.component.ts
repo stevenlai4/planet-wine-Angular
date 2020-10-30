c51a8d106c6a7980fa58ef9b6fe15cc6b40afc16
Steven <clai86@my.bcit.ca>
2020-10-30 16:31:36 -0700

    Add cart page #4

---
+++ planet-wine-angular/src/app/cart/cart.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import { fromEventPattern } from 'rxjs';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.scss'],
+})
+export class CartComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
