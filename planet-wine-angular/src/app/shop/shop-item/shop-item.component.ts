3292b58e03804aaa5373ea76f991c4680500b146
Steven <clai86@my.bcit.ca>
2020-10-30 16:31:11 -0700

    Shop "add" buttons funtionality #8

--- planet-wine-angular/src/app/shop/shop-item/shop-item.component.ts
+++ planet-wine-angular/src/app/shop/shop-item/shop-item.component.ts
@@ -78,4 +78,46 @@ export class ShopItemComponent implements OnInit {
       '../../assets/img/2019 Sauvignon Blanc.jpeg'
     ),
   ];
+
+  // Fill in value that have already been saved
+  fillInput(itemId: number): string {
+    if (localStorage.getItem(itemId.toString())) {
+      return localStorage.getItem(itemId.toString());
+    }
+
+    return '0';
+  }
+
+  // Click Event Listener for add button
+  addQuantityEvent(itemId: number, input: any) {
+    // Eliminating leading zero when there's one
+    let quantity: number = Number(input.value);
+    //Validation
+    if (isNaN(quantity)) {
+      window.alert(
+        'Please enter an integer number for the quantity and add it again!'
+      );
+    } else if (quantity < 0 || quantity > 100) {
+      window.alert(
+        'Please enter a quantity number between 0 to 100 and add it again!'
+      );
+      if (quantity < 0) {
+        input.value = 0;
+      } else {
+        input.value = 100;
+      }
+    } else if (!Number.isInteger(Number(quantity))) {
+      window.alert(
+        'Please enter an integer number for the quantity and add it again!'
+      );
+      quantity = Math.floor(quantity);
+    } else {
+      //Add item
+      if (quantity == 0) {
+        localStorage.removeItem(itemId.toString());
+      } else if (quantity > 0 && quantity <= 100) {
+        localStorage.setItem(itemId.toString(), quantity.toString());
+      }
+    }
+  }
 }
