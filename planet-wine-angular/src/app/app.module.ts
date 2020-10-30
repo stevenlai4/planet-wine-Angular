ea41d137e542bad3cb9d32a75bfa26d20bcab42b
Steven <clai86@my.bcit.ca>
2020-10-30 16:29:52 -0700

    Add router #10

--- planet-wine-angular/src/app/app.module.ts
+++ planet-wine-angular/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -6,6 +7,19 @@ import { MainNavComponent } from './main-nav/main-nav.component';
 import { MainFooterComponent } from './main-footer/main-footer.component';
 import { ShopComponent } from './shop/shop.component';
 import { ShopItemComponent } from './shop/shop-item/shop-item.component';
+import { CartComponent } from './cart/cart.component';
+import { CartItemComponent } from './cart-item/cart-item.component';
+
+const routes: Routes = [
+  {
+    path: 'shop',
+    component: ShopComponent,
+  },
+  {
+    path: 'cart',
+    component: CartComponent,
+  },
+];
 
 @NgModule({
   declarations: [
@@ -14,8 +28,10 @@ import { ShopItemComponent } from './shop/shop-item/shop-item.component';
     MainFooterComponent,
     ShopComponent,
     ShopItemComponent,
+    CartComponent,
+    CartItemComponent,
   ],
-  imports: [BrowserModule],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent],
 })
